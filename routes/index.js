5107be5685b69c2a69498e6bf2d7f2ebd30e6f24
BigJonas <jonasgaribay012@gmail.com>
2023-12-04 13:10:04 -0600

    Added landing page with no style or functionality.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index');
 });
+
+// TODO: Add navigation with other pages once they are co
+router.get('/contact-guess', function(req, res, next) {
+    // res.render();
+}); 
+// TODO: Add navigation with other pages once they are co
+router.get('/daily-challenge', function(req, res, next) {
+    // res.render();
+}); 
 
 module.exports = router;
