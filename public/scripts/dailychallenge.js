f74cdaf62f59e93828c335b0aa454f29b6372ab1
philbadcode <phillip.hail@yahoo.com>
2023-12-07 12:46:32 -0600

    added functionality

--- public/scripts/dailychallenge.js
+++ public/scripts/dailychallenge.js
@@ -1,13 +1,64 @@
+var locations = [
+    { town: "Pittsburg", state: "Kansas", areaCode: "620" },
+    { town: "Girard", state: "Kansas", areaCode: "620" },
+    { town: "Weir", state: "Kansas", areaCode: "620" },
+    { town: "Iola", state: "Kansas", areaCode: "620" },
+    { town: "Fort Scott", state: "Kansas", areaCode: "620" },
+    { town: "Chanute", state: "Kansas", areaCode: "620" },
+    { town: "Parsons", state: "Kansas", areaCode: "620" },
+    { town: "Baxter Springs", state: "Kansas", areaCode: "620" },
+    { town: "Independence", state: "Kansas", areaCode: "620" },
+    { town: "Arcadia", state: "Kansas", areaCode: "417" },
+    { town: "Joplin", state: "Missouri", areaCode: "417" },
+];
+
+var previousRandomIndex = -1;
+var correctAreaCode;
+
+function selectRandomLocation() {
+    var randomIndex;
+    do {
+        randomIndex = Math.floor(Math.random() * locations.length);
+    } while (randomIndex === previousRandomIndex);
+    var randomLocation = locations[randomIndex];
+    var locationTitle = document.querySelector(".location-title");
+    locationTitle.innerText = randomLocation.town + ", " + randomLocation.state;
+    correctAreaCode = randomLocation.areaCode;
+    previousRandomIndex = randomIndex;
+}
+
 function guess() {
-    var areaCode = document.getElementById("areaCodeInput").value;
+    var areaCodeInput = document.getElementById("areaCodeInput");
+    var areaCode = areaCodeInput.value;
+    if (areaCode === correctAreaCode) {
+        correct();
+    } else {
+        incorrect();
+    }
+    areaCodeInput.value = "";
+}
+
+function correct() {
     var resultMessage = document.getElementById("resultMessage");
-    resultMessage.innerText = "Entered Area Code: " + areaCode;
+    resultMessage.innerText = "Correct!";
+    openResultBox();
+    selectRandomLocation();
+}
+
+function incorrect() {
+    var resultMessage = document.getElementById("resultMessage");
+    resultMessage.innerText = "Incorrect";
+    openResultBox();
+}
+
+function openResultBox() {
     var resultBox = document.getElementById("resultBox");
     resultBox.classList.add("show");
-  }
-  
-  function closeResultBox() {
+}
+
+function closeResultBox() {
     var resultBox = document.getElementById("resultBox");
     resultBox.classList.remove("show");
-  }
-  
+}
+
+selectRandomLocation();
