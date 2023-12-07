99b69c83ae1d134f250a31a2211c461735692727
philbadcode <phillip.hail@yahoo.com>
2023-12-07 11:45:48 -0600

    updated routes for form

--- routes/dailychallenge.js
+++ routes/dailychallenge.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
     res.render('dailychallenge');
 });
 
+router.post('/guess', async (req, res) => {
+    try {
+        const areaCode = req.body.areaCode;
+        console.log('Received area code:', areaCode);
+        res.send(areaCode);
+    } catch (error) {
+        console.error('Error processing guess:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+
 module.exports = router;
